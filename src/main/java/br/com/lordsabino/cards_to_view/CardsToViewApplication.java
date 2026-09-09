362ea0a53d75403f015616f62f4db911db149d0e
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2026-09-09 07:44:49 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/lordsabino/cards_to_view/CardsToViewApplication.java
@@ -0,0 +1,13 @@
+package br.com.lordsabino.cards_to_view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsToViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsToViewApplication.class, args);
+	}
+
+}
